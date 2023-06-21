c1537198484fa2f2f6a0193a4a847dd7920184e2
Raveen Reddy <raveenreddy89@gmail.com>
2023-06-21 10:38:07 -0400

    search endpoint

---
+++ src/pages/api/search.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable import/no-anonymous-default-export */
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+import { getSpotifyToken } from "~/lib/getSpotifyToken";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { userId } = getAuth(req);
+  const { CLERK_SECRET_KEY = "" } = process.env;
+
+  const accessToken = await getSpotifyToken(userId ?? "", CLERK_SECRET_KEY);
+
+  const { query } = req.query;
+
+  const type = ["track"].join(",");
+
+  const endpointURL = "https://api.spotify.com/v1/search";
+
+  const finalURL = `${endpointURL}?q=${query}&type=${type}`;
+
+  const response = await fetch(finalURL, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (finalURL) {
+    const { tracks } = await response.json();
+    return res.status(200).json({
+      status: "OK",
+      data: tracks,
+    });
+  } else {
+    const { error } = await response.json();
+    return res.status(response.status).json({
+      status: "Error",
+      message: "errrorrrr",
+    });
+  }
+};
