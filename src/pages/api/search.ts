d04fac9c58fd00ddc787117ab3e39d8cc282ce94
Raveen Reddy <raveenreddy89@gmail.com>
2023-06-21 19:50:42 -0400

    endpoint types

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -5,6 +5,36 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { getAuth } from "@clerk/nextjs/server";
 import { getSpotifyToken } from "~/lib/getSpotifyToken";
 
+export type ApiResponse<T> = {
+  status: "OK" | "ERROR";
+  data?: T;
+  error?: string;
+};
+
+export type SpotifySearchResponse = {
+  href: string;
+  items: Array<{
+    album: SpotifyAlbum;
+    artists: SpotifyArtist[];
+    preview_url: string;
+    name: string;
+    uri: string;
+  }>;
+};
+
+export type SpotifyAlbum = {
+  name: string;
+  release_date: string;
+  href: string;
+  images: Array<{ url: string }>;
+  artists: Array<SpotifyArtist>;
+};
+
+export type SpotifyArtist = {
+  name: string;
+  external_urls: { spotify: string };
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { userId } = getAuth(req);
   const { CLERK_SECRET_KEY = "" } = process.env;
@@ -26,8 +56,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
-  if (finalURL) {
+  if (response.ok) {
     const { tracks } = await response.json();
+
     return res.status(200).json({
       status: "OK",
       data: tracks,
