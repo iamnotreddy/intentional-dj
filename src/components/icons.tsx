4c798aa9f904b4fcc780980f4e0543c4f79a9a8d
Raveen Reddy <raveenreddy89@gmail.com>
2023-06-22 11:56:40 -0400

    icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,4 @@
-export const MagnifyingGlass = () => {
+export const SearchIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +51,7 @@ export const PauseIcon = () => {
   );
 };
 
-export const PlusIcon = () => {
+export const AddIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -77,6 +77,23 @@ export const HeartIcon = () => {
       className="h-6 w-6"
     >
       <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+    </svg>
+  );
+};
+
+export const RemoveIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z"
+        clipRule="evenodd"
+      />
     </svg>
   );
 };
