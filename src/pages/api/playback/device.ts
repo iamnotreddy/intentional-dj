3d9557b5abe30ec2f052b60ae81c865019c3bc41
Raveen Reddy <raveenreddy89@gmail.com>
2023-06-26 20:51:16 -0400

    endpoints

--- src/pages/api/playback/device.ts
+++ src/pages/api/playback/device.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable import/no-anonymous-default-export */
+
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { getAuth } from "@clerk/nextjs/server";
 import { getSpotifyToken } from "~/lib/getSpotifyToken";
@@ -10,32 +11,35 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { CLERK_SECRET_KEY = "" } = process.env;
 
   const accessToken = await getSpotifyToken(userId ?? "", CLERK_SECRET_KEY);
-  const { query } = req.query;
 
-  const type = ["track"].join(",");
+  const { id } = req.query;
 
-  const endpointURL = "https://api.spotify.com/v1/search";
+  const deviceUrl = `https://api.spotify.com/v1/me/player/`;
 
-  const finalURL = `${endpointURL}?q=${query}&type=${type}`;
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  const requestBody = JSON.stringify({
+    device_ids: [id],
+  });
 
-  const response = await fetch(finalURL, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+  const response = await fetch(deviceUrl, {
+    method: "PUT",
+    headers: headers,
+    body: requestBody,
   });
 
   if (response.ok) {
-    const { tracks } = await response.json();
-
     return res.status(200).json({
       status: "OK",
-      data: tracks,
+      message: `connected to device ${typeof id === "string" ? id : ""}`,
     });
   } else {
     return res.status(response.status).json({
       status: "Error",
-      message: response.status,
+      message: response.statusText,
+      code: response.status,
+      body: response.body,
     });
   }
 };
--- src/pages/api/playback/play.ts
+++ src/pages/api/playback/play.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable import/no-anonymous-default-export */
+
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { getAuth } from "@clerk/nextjs/server";
 import { getSpotifyToken } from "~/lib/getSpotifyToken";
@@ -10,32 +11,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { CLERK_SECRET_KEY = "" } = process.env;
 
   const accessToken = await getSpotifyToken(userId ?? "", CLERK_SECRET_KEY);
-  const { query } = req.query;
 
-  const type = ["track"].join(",");
+  const { tracks } = req.query;
 
-  const endpointURL = "https://api.spotify.com/v1/search";
+  const trackArray =
+    tracks && typeof tracks === "string" ? tracks.split(",") : [];
 
-  const finalURL = `${endpointURL}?q=${query}&type=${type}`;
+  const baseUrl = `https://api.spotify.com/v1/me/player/play/`;
 
-  const response = await fetch(finalURL, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  const requestBody = JSON.stringify({
+    uris: trackArray.length > 0 ? trackArray : [""],
+  });
+
+  const response = await fetch(baseUrl, {
+    method: "PUT",
+    headers: headers,
+    body: requestBody,
   });
 
   if (response.ok) {
-    const { tracks } = await response.json();
-
     return res.status(200).json({
       status: "OK",
-      data: tracks,
+      message: "playing URIs:" + requestBody,
     });
   } else {
     return res.status(response.status).json({
       status: "Error",
-      message: response.status,
+      message: response.statusText,
+      code: response.status,
+      body: response.body,
     });
   }
 };
